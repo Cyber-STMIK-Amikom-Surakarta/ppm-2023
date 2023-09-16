7f45022134faa151457bdb4cdd88d4f9cea18bfe
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-09-16 10:07:24 +0700

    📦 Added effect on karya

--- src/slides/karya.svelte
+++ src/slides/karya.svelte
@@ -10,79 +10,179 @@
 	import CyberWeb1 from '@assets/karya/cyber-1.webp'
 	import CyberWeb2 from '@assets/karya/cyber-2.webp'
 	import CyberWeb3 from '@assets/karya/cyber-3.webp'
+
+	import TextPoppingEffect from '../components/TextPoppingEffect.svelte'
+	import TextLineSpawnEffect from '../components/TextLineSpawnEffect.svelte'
+
+	let animateKaryaTitle = 'NONE'
+	let animatePortfolioOnline = 'NONE'
+	let animateWebsiteECommerce = 'NONE'
+	let animateWebCyber = 'NONE'
 </script>
 
-<Slide animate>
+<Slide on:in={() => (animateKaryaTitle = 'ANIMATE')} animate>
 	<Layout>
-		<h1 class="text-[6rem] font-bold tracking-wider">Karya</h1>
+		<TextPoppingEffect
+			animate={animateKaryaTitle}
+			text="Karya"
+			class="text-[6rem] font-bold tracking-wider"
+		/>
 	</Layout>
 </Slide>
 
-<Slide animate>
+<Slide
+	on:in={() => {
+		animateKaryaTitle = 'ANIMATE'
+		animatePortfolioOnline = 'ANIMATE'
+	}}
+	on:out={() => (animatePortfolioOnline = 'REVERT')}
+	animate
+>
 	<Layout>
-		<h1 class="text-[2rem] font-bold tracking-wider">Karya</h1>
+		<TextPoppingEffect
+			animate={animateKaryaTitle}
+			text="Karya"
+			class="text-[2rem] font-bold tracking-wider"
+		/>
 		<a href="https://unknownrori.vercel.app">
 			<Stack>
 				<Image src={OnlinePortfolio1Gif} alt="Portofolio Online" />
 			</Stack>
 		</a>
-		<h2 class="text-6xl">Portfolio Online</h2>
+		<TextLineSpawnEffect
+			animate={animatePortfolioOnline}
+			text="Portfolio Online"
+			class="text-6xl"
+		/>
 	</Layout>
 </Slide>
 
-<Slide animate>
+<Slide
+	on:in={() => {
+		animateKaryaTitle = 'ANIMATE'
+		animateWebsiteECommerce = 'ANIMATE'
+	}}
+	animate
+>
 	<Layout>
-		<h1 class="text-[2rem] font-bold tracking-wider">Karya</h1>
+		<TextPoppingEffect
+			animate={animateKaryaTitle}
+			text="Karya"
+			class="text-[2rem] font-bold tracking-wider"
+		/>
 		<a href="https://github.com/UnknownRori/reservation-room-app">
 			<Stack>
 				<Image src={Schmhotel1} alt="Website E-Commerce" />
 			</Stack>
 		</a>
-		<h2 class="text-6xl">Website E-Commerce</h2>
+		<TextLineSpawnEffect
+			animate={animateWebsiteECommerce}
+			text="Website E-Commerce"
+			class="text-6xl"
+		/>
 	</Layout>
 </Slide>
 
-<Slide animate>
+<Slide
+	on:in={() => {
+		animateKaryaTitle = 'ANIMATE'
+		animateWebsiteECommerce = 'ANIMATE'
+	}}
+	on:out={() => {
+		animateWebsiteECommerce = 'REVERT'
+	}}
+	animate
+>
 	<Layout>
-		<h1 class="text-[2rem] font-bold tracking-wider">Karya</h1>
+		<TextPoppingEffect
+			animate={animateKaryaTitle}
+			text="Karya"
+			class="text-[2rem] font-bold tracking-wider"
+		/>
 		<a href="https://github.com/UnknownRori/reservation-room-app">
 			<Stack>
 				<Image src={Schmhotel1} alt="Website E-Commerce" />
 				<Image src={Schmhotel2} alt="Website E-Commerce" />
 			</Stack>
 		</a>
-		<h2 class="text-6xl">Website E-Commerce</h2>
+		<TextLineSpawnEffect
+			animate={animateWebsiteECommerce}
+			text="Website E-Commerce"
+			class="text-6xl"
+		/>
 	</Layout>
 </Slide>
 
-<Slide animate>
+<Slide
+	on:in={() => {
+		animateKaryaTitle = 'ANIMATE'
+		animateWebCyber = 'ANIMATE'
+	}}
+	animate
+>
 	<Layout>
-		<h1 class="text-[2rem] font-bold tracking-wider">Karya</h1>
+		<TextPoppingEffect
+			animate={animateKaryaTitle}
+			text="Karya"
+			class="text-[2rem] font-bold tracking-wider"
+		/>
 		<a href="https://cyberamikomsolo.netlify.app/">
 			<Stack>
 				<Image src={CyberWeb1} alt="Website Cyber" />
 			</Stack>
 		</a>
-		<h2 class="text-6xl">Website Cyber</h2>
+		<TextLineSpawnEffect
+			animate={animateWebCyber}
+			text="Web Cyber"
+			class="text-6xl"
+		/>
 	</Layout>
 </Slide>
 
-<Slide animate>
+<Slide
+	on:in={() => {
+		animateKaryaTitle = 'ANIMATE'
+		animateWebCyber = 'ANIMATE'
+	}}
+	animate
+>
 	<Layout>
-		<h1 class="text-[2rem] font-bold tracking-wider">Karya</h1>
+		<TextPoppingEffect
+			animate={animateKaryaTitle}
+			text="Karya"
+			class="text-[2rem] font-bold tracking-wider"
+		/>
 		<a href="https://cyberamikomsolo.netlify.app/">
 			<Stack>
 				<Image src={CyberWeb1} alt="Website Cyber" />
 				<Image src={CyberWeb2} alt="Website Cyber" />
 			</Stack>
 		</a>
-		<h2 class="text-6xl">Website Cyber</h2>
+		<TextLineSpawnEffect
+			animate={animateWebCyber}
+			text="Web Cyber"
+			class="text-6xl"
+		/>
 	</Layout>
 </Slide>
 
-<Slide animate>
+<Slide
+	on:in={() => {
+		animateKaryaTitle = 'ANIMATE'
+		animateWebCyber = 'ANIMATE'
+	}}
+	on:out={() => {
+		animateKaryaTitle = 'REVERT'
+		animateWebCyber = 'REVERT'
+	}}
+	animate
+>
 	<Layout>
-		<h1 class="text-[2rem] font-bold tracking-wider">Karya</h1>
+		<TextPoppingEffect
+			animate={animateKaryaTitle}
+			text="Karya"
+			class="text-[2rem] font-bold tracking-wider"
+		/>
 		<a href="https://cyberamikomsolo.netlify.app/">
 			<Stack>
 				<Image src={CyberWeb1} alt="Website Cyber" />
@@ -90,6 +190,10 @@
 				<Image src={CyberWeb3} alt="Website Cyber" />
 			</Stack>
 		</a>
-		<h2 class="text-6xl">Website Cyber</h2>
+		<TextLineSpawnEffect
+			animate={animateWebCyber}
+			text="Web Cyber"
+			class="text-6xl"
+		/>
 	</Layout>
 </Slide>
